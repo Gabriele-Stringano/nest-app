4972dbb62d6ce1272843c3d64c31634bf1a1e9b3
Mario Rossi <gabrielestringano@gmail.com>
2023-10-27 16:23:21 +0200

    Added prisma for Crud API and sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user.module';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { PostModule } from './post.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [HttpModule,UserModule,PostModule,ConfigModule.forRoot({
+    isGlobal: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/post.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { HttpModule } from "@nestjs/axios";
+import { PostService } from "./post.service";
+import { PostController } from "./post.controller";
+
+@Module({
+    imports: [HttpModule],
+    controllers: [PostController],
+    providers: [PostService],
+})
+
+export class PostModule{}
